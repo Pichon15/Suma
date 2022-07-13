2434571f5f3219c6dfd574101782b6c8cd7d009a
Luis Angel Martinez Hernandez <luis_pichon12@hotmail.com>
2022-07-12 20:25:36 -0500

    Este es el primer commit del archivo suma.py

---
+++ suma.py
@@ -0,0 +1,11 @@
+def suma(a:int,b:int):
+    return (a+b)
+
+def run():
+    print('Este programa suma dos numeros')
+    a = int(input('Inserta el primer numero: '))
+    b = int(input('Inserta el segundo numero: '))
+    print('el resultado es: {}'.format(suma(a,b)))
+
+if __name__ == '__main__':
+    run()
